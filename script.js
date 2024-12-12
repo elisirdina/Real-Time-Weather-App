d2fbfd40ba0423f181c1cc1f4c68331c6d9289b3
elisirdina <elisirdina.idr@gmail.com>
2024-12-12 10:09:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,48 @@ let cityInput = document.getElementById('city_input'),
 searchBtn = document.getElementById('searchBtn'),
 api_key = 'c014a189f4ff662dc25e56c2b086ae08';
 
+function getWeatherDetails(name, lat, lon, country, state){
+    letFORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={api_key}`,
+    WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={api_key}`,
+    days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ],
+    months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'June',
+        'July',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ];
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch(() => {
+        alert('Failed to fetch current weather');
+    });
+}
+
 function getCityCoordinates(){
     let cityName = cityInput.value.trim();
     cityInput.value = '';
     if(!cityName) return;
     let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid={api_key}`;
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        console.log(data);
+        let {name, lat, lon, country, state} = data[0];
+        getWeatherDetails(name, lat, lon, country, state);
     }).catch(() => {
         alert(`Failed to fetch coordinates of ${cityName}`);
     });
